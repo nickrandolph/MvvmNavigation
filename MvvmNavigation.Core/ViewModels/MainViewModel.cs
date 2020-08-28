55ff58bef91e268e9dda66193b7fe7fd9eb306e1
Nick Randolph <nick@builttoroam.com>
2020-08-29 09:58:15 +1000

    Add project files.

---
+++ MvvmNavigation.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MvvmNavigation.ViewModels
+{
+    public class MainViewModel
+    {
+        public event EventHandler ViewModelDone;
+
+        public string Title { get; } = "Main Page - VM";
+
+        public async Task<int> DoSomething()
+        {
+            var rnd = new Random().Next(1000);
+            await Task.Delay(rnd);
+            if (rnd % 2 == 0)
+            {
+                //ViewModelDone?.Invoke(this, EventArgs.Empty);
+            }
+            else
+            {
+                // Do something else
+            }
+            ViewModelDone?.Invoke(this, EventArgs.Empty);
+            return rnd;
+        }
+    }
+
+    public class SecondViewModel
+    {
+        public event EventHandler ViewModelDone;
+        public string Title { get; } = "Second Page - VM";
+
+        public void Done()
+        {
+            ViewModelDone?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ MvvmNavigation/MvvmNavigation.Shared/MainPage.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MvvmNavigation.ViewModels;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MvvmNavigation
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        //protected override void OnNavigatedTo(NavigationEventArgs e)
+        //{
+        //    base.OnNavigatedTo(e);
+        //    (DataContext as MainViewModel).ViewModelDone += MainViewModelDone;
+        //}
+
+        //protected override void OnNavigatedFrom(NavigationEventArgs e)
+        //{
+        //    (DataContext as MainViewModel).ViewModelDone -= MainViewModelDone;
+        //    base.OnNavigatedFrom(e);
+        //}
+
+        //private void MainViewModelDone(object sender, EventArgs e)
+        //{
+        //    Frame.Navigate(typeof(SecondPage));
+        //}
+
+        private void GoToSecondPageClick(object sender, RoutedEventArgs e)
+        {
+            (DataContext as MainViewModel)?.DoSomething();
+        }
+    }
+
+    public class NavigationRoutes
+    {
+        private IList<Tuple<Type, IApplicationBehaviour>> Behaviours { get; } = new List<Tuple<Type, IApplicationBehaviour>>();
+
+        public NavigationRoutes Register<T, THandler>(Action<T, THandler> init, Action<T, THandler> deinit, Func<INavigationService,THandler> actionFactory)
+        {
+            Behaviours.Add(new Tuple<Type, IApplicationBehaviour>(typeof(T), Behaviour<T,THandler>.Create(init, deinit, actionFactory)));
+            return this;
+        }
+
+        public void Wire(INavigationService navigationService, object page)
+        {
+            var typeOfPage = page.GetType();
+            var behaviours = Behaviours.Where(x => x.Item1 == typeOfPage).Select(x=>x.Item2);
+            foreach (var behaviour in behaviours)
+            {
+                behaviour.Attach(navigationService, page);
+            }
+        }
+
+        public void Unwire(object page)
+        {
+            var typeOfPage = page.GetType();
+            var behaviours = Behaviours.Where(x => x.Item1 == typeOfPage).Select(x => x.Item2);
+            foreach (var behaviour in behaviours)
+            {
+                behaviour.Detach();
+            }
+        }
+    }
+
+    public class Behaviour<T, THandler> :IApplicationBehaviour
+    {
+        private Action<T, THandler> Init { get; set; }
+        private Action<T, THandler> Deinit { get; set; }
+
+        private Func<INavigationService, THandler> ActionFactory { get; set; }
+
+        private T attachedEntity;
+        private THandler action;
+
+        public void Attach(INavigationService navService, object entity)
+        {
+            if(attachedEntity!=null)
+            {
+                Detach();
+            }
+
+            action = ActionFactory(navService);
+            attachedEntity = (T)entity;
+            Init(attachedEntity, action);
+        }
+
+        public void Detach()
+        {
+            if (attachedEntity == null)
+            {
+                return;
+            }
+
+            Deinit(attachedEntity, action);
+        }
+
+        private Behaviour() { }
+
+        public static Behaviour<T, THandler> Create(Action<T, THandler> init, Action<T, THandler> deinit,Func<INavigationService, THandler> actionFactory)
+        {
+            return new Behaviour<T, THandler>()
+            {
+                Init = init,
+                Deinit = deinit,
+                ActionFactory=actionFactory
+            };
+        }
+    }
+
+    public interface IApplicationBehaviour
+    {
+        void Attach(INavigationService navService, object entity);
+        void Detach();
+    }
+
+    public interface INavigationService
+    {
+        Task Navigate<TViewModel>();
+        Task GoBack();
+    }
+}
